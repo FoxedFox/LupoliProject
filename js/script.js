3b5d4718d8ec43bbe8157a989eb89e392e7e131e
FoxedFox <71232184+FoxedFox@users.noreply.github.com>
2023-02-17 09:46:51 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,7 +15,13 @@ new Swiper(".mySwiper", {
         navigation:{
 nextEl:'.swiper-button-next',
 prevEl:'.swiper-button-prev'
-    }});
+    },
+pagination:{
+ el:'.swiper-pagination',
+clickable: true,
+},
+
+});
 
 
 /* ПК-слайдер с брендами*/
